26fb4fe8d85cbea5beccc646bcbfc59a7ad955e1
Mustafa Nysan <mustafanysan@MacBook-Pro-Mustafa.local>
2023-01-13 21:15:20 +0600

    Initial commit

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from users.views import *
+from rest_framework import routers
+
+urlpatterns = [
+    path('', UserListView.as_view()),
+    path('<int:pk>/', UserDetailView.as_view()),
+    path('create/', UserCreateView.as_view()),
+    path('<int:pk>/update/', UserUpdateView.as_view()),
+    path('<int:pk>/delete/', UserDeleteView.as_view()),
+]
+
+router = routers.SimpleRouter()
+router.register('location', LocationViewSet)  # /user/location
+
+urlpatterns += router.urls
